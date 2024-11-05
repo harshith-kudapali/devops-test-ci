bc0fbedeb868d0aa01591542bc58e7275631be10
harshith_sk <122600784+harshith-kudapali@users.noreply.github.com>
2024-11-05 15:44:09 +0530

    commit

--- stu.py
+++ stu.py
@@ -2,7 +2,10 @@ my_list = [45, 68, 99, 34, 98, 55]
 sum = 0
 
 for i in my_list:
-    sum += i 
+    sum += i
 
-avg = float(sum) / len(my_list)  
-print("The average is " + str(avg)) 
+avg = float(sum) / len(my_list)
+print("The average is " + str(avg))
+
+with open('example.txt', 'w') as file:
+    file.write("The average is " + str(avg))
